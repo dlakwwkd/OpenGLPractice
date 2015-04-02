487d3c21f0e2674b8a33efbbfa323af71b293264
Subin Park <dlakek@naver.com>
2015-04-02 14:02:08 +0900

    사각형 그리기

--- test.cpp
+++ test.cpp
@@ -2,18 +2,36 @@
 #include <gl/glut.h>
 void RenderScene(void)
 {
-	glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glRectf(0.0f, 0.0f, 50.0f, 30.0f);
 	glFlush();
 }
 void SetupRC(void)
 {
     glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
 }
+void ChangeSize(GLsizei w, GLsizei h)
+{
+    glViewport(0, 0, w, h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+
+    if (w <= h)
+        glOrtho(-100.0f, 100.0f, -100.0f*h / w, 100.0f*h / w, 1.0f, -1.0f);
+    else
+        glOrtho(-100.0f*w / h, 100.0f*w / h, -100.0f, 100.0f, 1.0f, -1.0f);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+}
+
 int main()
 {
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	glutCreateWindow("Simple");
+	glutCreateWindow("Rectangle");
 	glutDisplayFunc(RenderScene);
+    glutReshapeFunc(ChangeSize);
 	SetupRC();
 	glutMainLoop();
 }
